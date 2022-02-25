79c83ef054f5ef15a60b8cee7b2ea5183629352d
Casey MacLeod <caseymacleod22@gmail.com>
2022-02-25 17:48:14 -0500

    backend tests

--- config/database.js
+++ config/database.js
@@ -1,11 +1,10 @@
-// TODO: establish a connection to mongoDB
 const mongoose = require('mongoose')
 
-const connectionURI = `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.esjr1.mongodb.net/teacherspet?retryWrites=true&w=majority`
+const connectionURI = `mongodb+srv://admin:pepper22@cluster0.esjr1.mongodb.net/teacherspet?retryWrites=true&w=majority`
 
 const db = mongoose.connection;
 
-module.exports = db
+// module.exports = db
 
 mongoose.connect(process.env.DATABASE_URI, {
     useNewUrlParser: true,
